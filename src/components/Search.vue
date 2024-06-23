e94df602b4218a2b7c32b968809dad59b2136d9b
Egor Chumichev <lightonce@yandex.com>
2024-06-23 21:49:38 +0500

    edit header layout

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -15,20 +15,20 @@ const emit = defineEmits(['search'])
 
 .search {
   display: flex;
-  justify-content: center;
+  width: 100%;
 }
 
 input {
   font-family: "Rubik", sans-serif;
   font-weight: 400;
   font-size: 16px;
+  line-height: 24px;
   background-color: #F1F1F1;
-  width: 1000px;
-  height: 45px;
+  width: 100%;
   border: none;
   border-radius: 7px;
   outline: none;
-  padding: 10px 15px;
+  padding: 12px 16px;
 }
 
 </style>
